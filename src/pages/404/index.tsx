d9ccfeac99deebe323baa7b8f30a6c99abbd1de8
liuzhe <1804319025@qq.com>
2025-02-04 15:16:15 +0800

    fix

--- src/pages/404/index.tsx
+++ src/pages/404/index.tsx
@@ -1,5 +1,12 @@
+import './index.scss';
+
 function Error() {
-	return <div>404</div>;
+	return (
+		<>
+			<div>404</div>
+			<p className="style-isolated">样式隔离</p>
+		</>
+	);
 }
 
 export default Error;
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,10 +3,13 @@ import './index.scss';
 
 function Home() {
 	return (
-		<div className="title">
-			<img src={reactLogo} alt="" />
-			小哲自制react模板
-		</div>
+		<>
+			<div className="title">
+				<img src={reactLogo} alt="" />
+				小哲自制react模板
+			</div>
+			<p className="style-isolated">样式隔离</p>
+		</>
 	);
 }
 
